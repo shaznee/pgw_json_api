efc29ee285c9720595d7dcb178ffd9cef75fa0d9
Steve Copous <steve.copous@firstdata.com>
2015-08-05 15:25:41 -0500

    Added serializer for DCC Indicator types.

---
+++ src/main/java/com/firstdata/payeezygateway/serializers/DCCIndicatorTypeSerializer.java
@@ -0,0 +1,18 @@
+package com.firstdata.payeezygateway.serializers;
+
+import java.lang.reflect.Type;
+
+import com.firstdata.payeezygateway.DCCIndicatorType;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class DCCIndicatorTypeSerializer implements JsonSerializer<DCCIndicatorType> {
+	/* (non-Javadoc)
+	 * @see com.google.gson.JsonSerializer#serialize(java.lang.Object, java.lang.reflect.Type, com.google.gson.JsonSerializationContext)
+	 */
+	public JsonElement serialize(DCCIndicatorType src, Type typeOfSrc, JsonSerializationContext context) {
+		return new JsonPrimitive(src.toString());
+	}
+}
